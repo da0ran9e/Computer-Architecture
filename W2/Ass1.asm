10cb274fbae28bb8fcf09af3d2f044330e39ce4b
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-09-28 09:24:48 +0700

    Update Ass1.asm

--- W2/Ass1.asm
+++ W2/Ass1.asm
@@ -1,17 +1,11 @@
+.data
+
+x: .byte 256
+
 .text
 
 # Assign X, Y
 
-addi $t1, $zero, 4 # X = $t1 = ?
+la $t1, x
 
-addi $t2, $zero, 5 # Y = $t2 = ?
-
-# Expression Z = 3*XY
-
-mul $s0, $t1, $t2 # HI-LO = $t1 * $t2 = X * Y ; $s0 = LO
-
-mul $s0, $s0, 3 # $s0 = $s0 * 3 = 3 * X * Y
-
-# Z' = Z
-
-mflo $s1
+lb $t2, 0($t1)
