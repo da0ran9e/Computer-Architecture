26a71b67195a543e2e08a1744a73b1847e809703
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-09-28 09:20:21 +0700

    Update Ass1.asm

--- W2/Ass1.asm
+++ W2/Ass1.asm
@@ -1,4 +1,17 @@
-#Laboratory Exercise 2, Assignment 1
 .text
-addi $s0, $zero, 0x2110003d # $s0 = 0 + 0x3007 = 0x3007 ;I-type
-add $s0, $zero, $0 # $s0 = 0 + 0 = 0 ;R-type
+
+# Assign X, Y
+
+addi $t1, $zero, 4 # X = $t1 = ?
+
+addi $t2, $zero, 5 # Y = $t2 = ?
+
+# Expression Z = 3*XY
+
+mul $s0, $t1, $t2 # HI-LO = $t1 * $t2 = X * Y ; $s0 = LO
+
+mul $s0, $s0, 3 # $s0 = $s0 * 3 = 3 * X * Y
+
+# Z' = Z
+
+mflo $s1
