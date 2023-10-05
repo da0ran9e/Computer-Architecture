651da64560e96901378d6f6f62e8aaf9d537ccc5
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-05 07:44:16 +0700

    Update if-else.asm

--- W3/if-else.asm
+++ W3/if-else.asm
@@ -1,16 +1,26 @@
-#
-.text
-    #s0 = a
-    #s1 = b
-    #s2 = maximum
+# if (i <= j)
+#   x=x+1;
+#   z=1;
+#else 
+#   y=y-1;
+#   z=2*z
 
-    slt $t0, $s0, $s1
+.text
+    #s0 = i
+    #s1 = j
+    #t1 = x
+    #t2 = y
+    #t3 = z
+
+    slt $t0, $s2, $s1 #select less than
     je $t0, 0, ELSE
 
-    addi $s2, $s1, 0
+    addi $t1, $t1, 1 #x = x+1
+    addi $t3, $zero, 1 #z=1
     j CONTINUE
 
 ELSE:
-    addi $s2, $s0, 0
+    addi $t2, $t0, -1
+    add $t3, $t3, $t3 #z = 2*z
 
 CONTINUE:
