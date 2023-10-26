2417493d1be1f8c49af17502e78d178c15d1a9d0
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-26 08:03:03 +0700

    Update Ass3.asm

--- W6/Ass3.asm
+++ W6/Ass3.asm
@@ -1,38 +1,32 @@
-#Laboratory Exercise 5, Assignment 2
 .data
-e1: 	.asciiz "Enter the sring: "
-e2: 	.asciiz "Copy of the string: "
-x: 	.space 1000
-y: 	.space 1000
+A: .word 7, -2, 5, 1, 5, 6, 7, 3, 6, 8, 8, 59 ,5
+Aend: .word 
 .text
-	li 	$v0, 4 
-        la 	$a0, e1  
-        syscall 
-	li 	$v0, 8
-	la	$a0, y
-	li 	$a1,1000
-	syscall
-	la	$a1, y
-	la 	$a0, x
-strcpy:
-	add 	$s0,$zero,$zero
-L1:
-	add 	$t1,$s0,$a1
-
-	lb  	$t2,0($t1) 
-	add 	$t3,$s0,$a0 
-
-	sb 	$t2,0($t3) 
-	beq 	$t2,$zero,end_of_strcpy
-	nop
-	addi 	$s0,$s0,1 
-	j 	L1 
-	nop
-end_of_strcpy:
-	li 	$v0, 4 
-        la 	$a0, e2 
-        syscall 
-	li 	$v0, 4
-    	la 	$a0, x
-    	syscall                
-    
+main:	
+	la 	$a0, A       	#$a0 = Address(A[0])
+	addi	$a0,$a0,-4
+	la 	$a1, Aend
+	addi 	$a1, $a1, -4   	#$a1 = Address(A[n-1])
+	j	sort            #sort
+after_sort:
+	li	$v0, 10
+	syscall 
+end_main:
+sort:
+	addi 	$a0, $a0, 4	#increase address of a0 by 4
+	beq 	$a0, $a1, after_sort #if $a[0] = $a[n-1] end loop 
+	addi 	$a2, $a0, 0	
+	j	loop
+loop:
+	beq	$a2, $a1, sort
+	lw	$v0, 0($a0)
+	addi	$a2, $a2, 4 
+	lw	$v1, 0($a2)
+	slt	$t3, $v1, $v0
+	bne	$t3, $zero, swap
+	j	loop	
+swap:   
+	lw	$t0,0($a0)
+	sw	$t0,0($a2)
+	sw	$v1,0($a0)
+	j	loop
