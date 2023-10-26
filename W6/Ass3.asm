9c7d739da263ce22d72b8d24dafe74ad927d73b3
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-26 09:30:26 +0700

    Update Ass3.asm

--- W6/Ass3.asm
+++ W6/Ass3.asm
@@ -3,18 +3,18 @@ A: .word 7, -2, 5, 1, 5, 6, 7, 3, 6, 8, 8, 59 ,5
 Aend: .word 
 .text
 main:	
-	la 	$a0, A       	#$a0 = Address(A[0])
+	la 	$a0, A       	
 	addi	$a0,$a0,-4
 	la 	$a1, Aend
-	addi 	$a1, $a1, -4   	#$a1 = Address(A[n-1])
-	j	sort            #sort
+	addi 	$a1, $a1, -4   	
+	j	sort            
 after_sort:
 	li	$v0, 10
 	syscall 
 end_main:
 sort:
-	addi 	$a0, $a0, 4	#increase address of a0 by 4
-	beq 	$a0, $a1, after_sort #if $a[0] = $a[n-1] end loop 
+	addi 	$a0, $a0, 4	#i++
+	beq 	$a0, $a1, after_sort 
 	addi 	$a2, $a0, 0	
 	j	loop
 loop:
