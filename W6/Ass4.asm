20ab9fc7460a23b50df358270cd9ac591ddf8d86
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-26 09:30:27 +0700

    Update Ass4.asm

--- W6/Ass4.asm
+++ W6/Ass4.asm
@@ -1,32 +1,41 @@
 .data
-A: 	.word 7, -2, 5, 1, 5, 6, 7, 3, 6, 8, 8, 59, 5
-Aend: 	.word
+A: .word 7, -2, 5, 1, 5, 6, 7, 3, 6, 8, 8, 59 ,5
+Aend: .word
+
 .text
-main: 	
-	la	$a0,A
-	la 	$t0,Aend
-	addi	$a0,$a0,-4
-	addi	$t1,$t0,-4
-loop1:	
-	addi	$a1,$a0,4
-	addi 	$a0,$a0,4
-	beq	$a0,$t0,end
-	nop
-loop2:
-	lw	$s0,0($a0)
-	lw	$s1,0($a1)
-	bgt	$s0,$s1,sort
-	j 	next
-	nop
-sort:
-	sw	$s1,0($a0)
-	sw	$s0,0($a1)
-	j 	next
-	nop
-next:
-	beq	$a1,$t1,loop1
-	addi	$a1,$a1,4
-	j 	loop2
-	nop
-end:
-	
+main:
+    la $a0, A           
+    la $a1, Aend        
+    addi $t0, $a0, 4    
+    j insertion_sort    
+
+after_sort:
+    li $v0, 10           # Exit
+    syscall
+
+insertion_sort:
+    add $v0, $t0, $zero  # Copy the address of the current element into $v0
+
+loop:
+    lw $s0, ($v0)       
+    lw $s1, -4($v0)     
+    slt $s2, $s0, $s1   
+
+    beq $s2, $zero, skip_swap  
+
+    sw $s0, -4($v0)      # Swap current and previous elements
+    sw $s1, ($v0)
+
+    subi $v0, $v0, 4     # Move to A[i-1]
+    beq $v0, $a0, skip_swap  
+    j loop
+
+skip_swap:
+    addi $t0, $t0, 4    # Move to the next element
+
+    beq $t0, $a1, end_sort  #
+
+    j insertion_sort     
+
+end_sort:
+    j after_sort          # Jump to end 
