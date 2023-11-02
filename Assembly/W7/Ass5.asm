7bfebff1d15edb16e3de060cd801c1b12dfb3d99
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-11-02 10:37:07 +0700

    Update Ass5.asm

--- Assembly/W7/Ass5.asm
+++ Assembly/W7/Ass5.asm
@@ -3,10 +3,9 @@
     MesOfMin:    .asciiz "The smallest value is "
     MesMaxIndex: .asciiz "The largest element is stored in $s"
     MesMinIndex: .asciiz "The smallest element is stored in $s"
-
 .text
 main:
-    # Initialize variables for values and indices
+    # Initialize variables
     li $s0, -1
     li $s1, 3
     li $s2, 1
@@ -15,12 +14,9 @@ main:
     li $s5, 4
     li $s6, -5
     li $s7, 6
-
-    # Call the initialization function
     jal init
     nop
-
-    # Print the result for the maximum value and its index
+    # Print max
     li $v0, 56
     la $a0, MesMaxIndex
     add $a1, $t8, $zero
@@ -28,33 +24,27 @@ main:
     la $a0, MesOfMax
     add $a1, $t0, $zero
     syscall
-
-    # Print the result for the minimum value and its index
+    # Print min
     la $a0, MesMinIndex
     add $a1, $t9, $zero
     syscall
     la $a0, MesOfMin
     add $a1, $t1, $zero
     syscall
-
-    # Exit the program
-    li $v0, 10
+    li $v0, 10 # Exit
     syscall
-
 swapMax:
-    # Swap Max and its index
+    # Swap Max
     add $t0, $t3, $zero
     add $t8, $t2, $zero
     jr $ra
-
 swapMin:
-    # Swap Min and its index
+    # Swap Min 
     add $t1, $t3, $zero
     add $t9, $t2, $zero
     jr $ra
-
 init:
-    # Set up the stack frame and initialize variables
+    # Set up the stack 
     add $fp, $sp, $zero
     addi $sp, $sp, -32
     sw $s1, 0($sp)
@@ -64,45 +54,26 @@ init:
     sw $s5, 16($sp)
     sw $s6, 20($sp)
     sw $s7, 24($sp)
-    sw $ra, 28($sp)
-    
-    # Initialize Max and Min to the lowest possible value
+    sw $ra, 28($sp)   
+    # Initialize Max and Min 
     add $t0, $s0, $zero
-    add $t1, $s0, $zero
-    
-    # Initialize indices for Max and Min
+    add $t1, $s0, $zero 
+    # Initialize indexes of Max and Min
     li $t8, 0
-    li $t9, 0
-    
-    # Initialize the current index
-    li $t2, 0
-
-max_min:
-    # Increase the stack pointer
-    addi $sp, $sp, 4
-    
-    # Load the current array element
+    li $t9, 0   
+    li $t2, 0 #current index
+max_min: 
+    addi $sp, $sp, 4 # move pointer 4 bits
     lw $t3, -4($sp)
-    
-    # Check if we've reached the end (based on the stack frame)
-    sub $t4, $sp, $fp
+    sub $t4, $sp, $fp # Check if we reached the end  or not
     beq $t4, $zero, done
-    
-    # Increment the current index
-    addi $t2, $t2, 1
-    
-    # Compare the current element with Max and Min
+    addi $t2, $t2, 1 # i++
+    # Compare
     sub $t4, $t0, $t3
     bltzal $t4, swapMax
     sub $t4, $t3, $t1
     bltzal $t4, swapMin
-    
-    # Continue with the next element
-    j max_min
-
+    j max_min    # Continue
 done:
-    # Restore the return address
     lw $ra, -4($sp)
-    
-    # Return to the calling program
     jr $ra
