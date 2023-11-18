c670818b5aa4b2fa560cb35f0eae17c94de3f2a4
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-11-18 21:21:53 +0700

    Update Ass5.asm

--- W5/Ass5.asm
+++ W5/Ass5.asm
@@ -1,59 +1,53 @@
+#Laboratory Exercise 5, Assignment 2
 .data
-text1: .asciiz "Enter string: "
-text2: .asciiz "Reversed string: "
-newline: .asciiz "\n"
-input: .space 20
+e1: 	.asciiz "Enter the string: "
+e2: 	.asciiz "\nThe reverse string: "
+x: 	.space 21
+y: 	.space 21
+.text	
+	li 	$v0, 4 
+        la 	$a0, e1  
+        syscall 
+	li 	$v0, 8
+	la	$a0, y
+	li 	$a1,21
+	syscall
 
-.text
-    main:
-    print_text:
-        li $v0, 4
-        la $a0, text1
-        syscall
-
-    get_string:
-        li $v0, 8
-        la $a0, input
-        li $a1, 20
-        syscall
-
-    get_length: 
-        la $t0, input
-        li $t1, 0
-
-    loop:
-        lb $t2, 0($t0)       # Load byte from the string
-        beqz $t2, end_input  # end input
-
-        li $t3, 10
-        beq $t2, $t3, continue
-
-        addi $t0, $t0, 1     # t0++ size of poiter
-        addi $t1, $t1, 1     # t1++ counter
-        bnez $t1, loop
-
-    continue:
-        addi $t0, $t0, 1     #pointer size +1 but string length does not
-        j loop
-
-    end_input:
-        li $v0, 4
-        la $a0, text2
-        syscall
-
-        la $t0, input
-        add $t0, $t0, $t1  # Pointer point to the end of the string
-
-    print_reverse:
-        beqz $t1, END  # end if printed entire string
-        subi $t0, $t0, 1  # Move 1 character
-        lb $t2, 0($t0)   # Load byte from the string
-        li $v0, 11
-        move $a0, $t2     # Print
-        syscall
-        subi $t1, $t1, 1  # t1--
-        j print_reverse
-
-    END:
-        li $v0, 10
-        syscall
+get_length: 	
+	la 	$a0, y
+	xor 	$v0, $zero, $zero 
+	xor 	$t0, $zero, $zero
+check_char: 	
+	add 	$t1, $a0, $t0 
+	lb 	$t2, 0($t1)
+	beq 	$t2,$zero,end_of_str 
+	addi 	$v0, $v0, 1 
+	addi 	$t0, $t0, 1 
+	j 	check_char
+end_of_str:
+end_of_get_length:
+	sub 	$v0,$v0,1
+	move	$s2,$v0
+	la	$a1, y
+	la 	$a0, x
+strrv:
+	add 	$s0,$zero,$0
+L1:
+	add 	$t1,$0,$a1
+	add	$t1,$t1,$s2
+	lb  	$t2,0($t1) 
+	add 	$t3,$s0,$a0 
+	sb 	$t2,0($t3) 
+	beq 	$s2,$zero,end_of_strrv
+	nop
+	addi 	$s0,$s0,1 
+	subi	$s2,$s2,1
+	j 	L1 	
+end_of_strrv:
+	li 	$v0, 4 
+        la 	$a0, e2
+        syscall 
+	li 	$v0, 4
+    	la 	$a0, x
+    	syscall                
+    
