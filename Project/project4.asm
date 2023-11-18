46dc0aae344e5657089e3cf87408ebea1752e8f2
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-11-18 22:20:01 +0700

    Update project4.asm

--- Project/project4.asm
+++ Project/project4.asm
@@ -1,41 +1,76 @@
 .data
-    prompt_size: .asciiz "Enter the size of the array (n): "
-    prompt_arr:  .asciiz "Enter element #"
-    array:       .space 1000  # Adjust the size as needed
+    prompt:     .asciiz "Enter the number of elements in the array: "
+    arrayPrompt: .asciiz "Enter element "
+    maxMessage:  .asciiz "The maximum element is: "
 
 .text
-    main:
-        # Prompt for array size
-        li $v0, 4
-        la $a0, prompt_size
-        syscall
+main:
+    # Prompt for the number of elements in the array
+    li $v0, 4
+    la $a0, prompt
+    syscall
 
-        # Read array size from user
-        li $v0, 5
-        syscall
-        move $t1, $v0   # $t1 now contains the size of the array
+    # Read the number of elements
+    li $v0, 5
+    syscall
+    move $t0, $v0 # $t0 = number of elements
 
-        # Prompt for array elements
-        la $t0, array     # Load the base address of the array
-        li $t2, 0         # Initialize loop counter
+    # Check if the array is empty
+    beq $t0, $zero, end_program
 
-    input_loop:
-        beq $t2, $t1, done_input
-        li $v0, 4
-        la $a0, prompt_arr
-        syscall
+    # Allocate space for the array on the stack
+    addi $sp, $sp, -4
+    sw $t0, 0($sp) # Save the number of elements on the stack
 
-        # Read array element from user
-        li $v0, 5
-        syscall
-        sw $v0, 0($t0)   # Store the element in the array
-        addi $t0, $t0, 4 # Move to the next array element
-        addi $t2, $t2, 1 # Increment loop counter
-        j input_loop
+    # Dynamically allocate space for the array
+    li $v0, 9
+    move $a0, $t0
+    syscall
+    move $t1, $v0 # $t1 = address of the array
 
-    done_input:
-        # Your further processing logic goes here
+    # Initialize variables
+    li $t2, 0      # $t2 = current index
+    li $t3, -99999 # $t3 = max element, initialize with a small value
 
-        # Exit the program
-        li $v0, 10
-        syscall
+    read_loop:
+    # Prompt for array element
+    li $v0, 4
+    la $a0, arrayPrompt
+    syscall
+    move $a0, $t2
+    li $v0, 1
+    syscall
+
+    # Read array element
+    li $v0, 5
+    syscall
+    sw $v0, 0($t1) # Store the element in the array
+
+    # Compare with the current max
+    bge $v0, $t3, update_max
+
+    # Move to the next index
+    j next_iteration
+
+    update_max:
+    move $t3, $v0
+
+    next_iteration:
+    # Move to the next index
+    addi $t2, $t2, 1
+
+    # Check if all elements have been read
+    bne $t2, $t0, read_loop
+
+    # Print the maximum element
+    li $v0, 4
+    la $a0, maxMessage
+    syscall
+    move $a0, $t3
+    li $v0, 1
+    syscall
+
+    end_program:
+    # Exit the program
+    li $v0, 10
+    syscall
