af8fd35ecb0b83695a225545ce20b3060afedd92
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-11-22 19:55:34 +0700

    Update project4.asm

--- Project/project4.asm
+++ Project/project4.asm
@@ -4,8 +4,8 @@
     colon:		        .asciiz ": "
     findPrompt:     	.asciiz "Enter the number to find: "
     maxMessage:     	.asciiz "\nThe maximum element is: "
-    nInputMessage:	    .asciiz "\nEnter n: "
-    mInputMessage:	    .asciiz "Enter m: "
+    mInputMessage:	    .asciiz "\nEnter n: "
+    MInputMessage:	    .asciiz "Enter m: "
     returnMessage1:   	.asciiz "The number of elements between "
     returnMessage2:   	.asciiz " and "
     returnMessage3:   	.asciiz " is: "
@@ -106,113 +106,116 @@ main:
 #       - If any value of m or M is not found then exit the program
 #---------------------------------------------------------------------
 
-    #run to the bottom of the stack
-    addi $sp,$sp,-8
+    #run the pointer to the bottom of the stack
+        addi $sp,$sp,-8 
+
     stack_loop:
-    addi $sp,$sp,4 #adjust the stack 
-    stack_pointer_iteration:
-    addi $t2, $t2, -1
-    bne $t2, $zero, stack_loop
+        addi $sp,$sp,4 # move the poiter 1 word
+        addi $t2, $t2, -1 # index--
+        bne $t2, $zero, stack_loop # the index return to 0
     
     #begin input
-    li $v0, 4
-    la $a0, nInputMessage
-    syscall
+        li $v0, 4
+        la $a0, mInputMessage
+        syscall
+        
+        li $v0, 5
+        syscall
+        move $t4, $v0 # $t4 = m
+        
+        li $v0, 4
+        la $a0, MInputMessage
+        syscall
     
-    li $v0, 5
-    syscall
-    move $t4, $v0 # $t4 = n
-    
-    li $v0, 4
-    la $a0, mInputMessage
-    syscall
-    
-    li $v0, 5
-    syscall
-    move $t5, $v0 # $t5 = m
+        li $v0, 5
+        syscall
+        move $t5, $v0 # $t5 = M
     #End of input
     
     #Find for values
-    find_n_loop:
-    lw $t1,4($sp) #pop the stack value to t1
-    beq $t1, $t4, found_n
-    
-    addi $sp,$sp,-4
-
-    addi $t2, $t2, 1
-    bne $t2, $t0, find_n_loop
-    
-    not_found_n:
-    li $v0, 4
-    la $a0, notfoundMessage
-    syscall
-    
-    move $a0, $t4
-    li $v0, 1
-    syscall
-    
-    li $v0, 4
-    la $a0, endl
-    syscall
-    
-    j end_program
-    
-    found_n:
-    move $t6, $t2 #save the found index to $t6
-    j find_m_loop
-    
-    #find m
     find_m_loop:
-    lw $t1,4($sp) #pop the stack value to t1
-    beq $t1, $t5, found_m
+        lw $t1,4($sp) #get the stack value to t1
+        beq $t1, $t4, found_m
     
-    addi $sp,$sp,-4 #adjust the stack pointer
+        addi $sp,$sp,-4 #adjust the stack pointer
 
-    addi $t2, $t2, 1
-    bne $t2, $t0, find_m_loop
-    
-    not_found_m:
-    li $v0, 4
-    la $a0, notfoundMessage
-    syscall
-    
-    move $a0, $t5
-    li $v0, 1
-    syscall
-    
-    li $v0, 4
-    la $a0, endl
-    syscall
-    
-    j end_program
+        addi $t2, $t2, 1 # index++
+        bne $t2, $t0, find_m_loop # while (index != n): loop
+
+    #if the find loop of m run to here that mean m is not found
+    not_found_m: 
+        li $v0, 4
+        la $a0, notfoundMessage
+        syscall
+        
+        move $a0, $t4
+        li $v0, 1
+        syscall
+        
+        li $v0, 4
+        la $a0, endl
+        syscall
+        
+        j end_program # exit the program
     
     found_m:
-    sub $t7, $t2, $t6
-    sub $t7, $t7, 1
+        move $t6, $t2 #save the found index to $t6
+        j find_M_loop
+    
+    #find M
+    find_M_loop:
+        lw $t1,4($sp) #get the stack value to t1
+        beq $t1, $t5, found_M
+    
+        addi $sp,$sp,-4 #adjust the stack pointer
+
+        addi $t2, $t2, 1 # index++
+        bne $t2, $t0, find_M_loop # while (index != n): loop
+    
+    # if M is not found in the rest of the array
+    not_found_M:
+        li $v0, 4
+        la $a0, notfoundMessage
+        syscall
+        
+        move $a0, $t5
+        li $v0, 1
+        syscall
+        
+        li $v0, 4
+        la $a0, endl
+        syscall
+        
+        j end_program # exit program
+    
+    # calculate the number of elements
+    found_M:
+        sub $t7, $t2, $t6
+        sub $t7, $t7, 1
     
     return:
-    li $v0, 4
-    la $a0, returnMessage1
-    syscall
-    
-    move $a0, $t4
-    li $v0, 1
-    syscall
-    
-    li $v0, 4
-    la $a0, returnMessage2
-    syscall
-    
-    move $a0, $t5
-    li $v0, 1
-    syscall
-    
-    li $v0, 4
-    la $a0, returnMessage3
-    syscall
-    
-    move $a0, $t7
-    li $v0, 1
-    syscall
-    
+        li $v0, 4
+        la $a0, returnMessage1
+        syscall
+        
+        move $a0, $t4
+        li $v0, 1
+        syscall
+        
+        li $v0, 4
+        la $a0, returnMessage2
+        syscall
+        
+        move $a0, $t5
+        li $v0, 1
+        syscall
+        
+        li $v0, 4
+        la $a0, returnMessage3
+        syscall
+        
+        move $a0, $t7
+        li $v0, 1
+        syscall
+        
     end_program:
