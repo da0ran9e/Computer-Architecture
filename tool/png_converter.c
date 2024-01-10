abe6d5a7cdbb4ba0016be8af25abf8b512f06c99
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2024-01-10 21:02:34 +0700

    Update png_converter.c

--- tool/png_converter.c
+++ tool/png_converter.c
@@ -1,28 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 #include <png.h>
 
-void read_png_file(const char *filename) {
-    png_byte header[8];
-    FILE *fp = fopen(filename, "rb");
-    if (!fp)
-        abort();
+void read_png_file(const char *filename);
 
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <png_file>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    read_png_file(argv[1]);
+
+    return EXIT_SUCCESS;
+}
+
+void read_png_file(const char *filename) {
+    FILE *fp = fopen(filename, "rb");
+    if (!fp) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    png_byte header[8];
     fread(header, 1, 8, fp);
 
-    if (png_sig_cmp(header, 0, 8))
-        abort();
+    if (png_sig_cmp(header, 0, 8)) {
+        fprintf(stderr, "Error: %s is not a PNG file\n", filename);
+        fclose(fp);
+        exit(EXIT_FAILURE);
+    }
 
     png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    if (!png)
-        abort();
+    if (!png) {
+        fclose(fp);
+        exit(EXIT_FAILURE);
+    }
 
     png_infop info = png_create_info_struct(png);
-    if (!info)
-        abort();
-
-    if (setjmp(png_jmpbuf(png)))
-        abort();
+    if (!info) {
+        png_destroy_read_struct(&png, (png_infopp)NULL, (png_infopp)NULL);
+        fclose(fp);
+        exit(EXIT_FAILURE);
+    }
 
     png_init_io(png, fp);
     png_set_sig_bytes(png, 8);
@@ -46,51 +68,34 @@ void read_png_file(const char *filename) {
     if (png_get_valid(png, info, PNG_INFO_tRNS))
         png_set_tRNS_to_alpha(png);
 
-    if (color_type == PNG_COLOR_TYPE_RGB ||
-        color_type == PNG_COLOR_TYPE_GRAY ||
-        color_type == PNG_COLOR_TYPE_PALETTE)
+    if (color_type == PNG_COLOR_TYPE_RGB || color_type == PNG_COLOR_TYPE_GRAY || color_type == PNG_COLOR_TYPE_PALETTE)
         png_set_filler(png, 0xFF, PNG_FILLER_AFTER);
 
-    if (color_type == PNG_COLOR_TYPE_GRAY ||
-        color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
+    if (color_type == PNG_COLOR_TYPE_GRAY || color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
         png_set_gray_to_rgb(png);
 
     png_read_update_info(png, info);
 
-    png_bytep *row_pointers = (png_bytep *)malloc(sizeof(png_bytep) * height);
-    for (int y = 0; y < height; y++) {
+    png_bytep row_pointers[height];
+    for (int y = 0; y < height; y++)
         row_pointers[y] = (png_byte *)malloc(png_get_rowbytes(png, info));
-    }
 
     png_read_image(png, row_pointers);
 
     fclose(fp);
 
     // Convert pixel data to binary string
-    printf("const char *binary_data = \"");
+    printf("const char *binary_string = \"");
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            printf("\\x%02X%02X%02X%02X", row_pointers[y][x * 4], row_pointers[y][x * 4 + 1], row_pointers[y][x * 4 + 2], row_pointers[y][x * 4 + 3]);
-            if (!(x == width - 1 && y == height - 1))
-                printf(", ");
+            png_bytep px = &(row_pointers[y][x * 4]);
+            printf("\\x00%02X%02X%02X%02X, ", px[0], px[1], px[2], px[3]);
         }
     }
     printf("\";\n");
 
-    // Free memory
-    for (int y = 0; y < height; y++) {
+    for (int y = 0; y < height; y++)
         free(row_pointers[y]);
-    }
-    free(row_pointers);
-}
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <png_file>\n", argv[0]);
-        return 1;
-    }
-
-    read_png_file(argv[1]);
-
-    return 0;
+    png_destroy_read_struct(&png, &info, NULL);
 }
